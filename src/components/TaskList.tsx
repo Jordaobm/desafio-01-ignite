4d471046d373e274c18eb00d0376ff7db474fec1
Jordão Beghetto Massariol <jbmassariol@gmail.com>
2021-03-14 13:17:28 -0300

    Finalizando Desafio 01

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,37 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [localId, setLocalId] = useState(0);
+
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle){
+      return ;
+    }
+
+    const newTask:Task = {
+      id:localId+1,
+      isComplete:false,
+      title:newTaskTitle,
+    }
+    setLocalId(localId+1)
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task=>task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task
+    )
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(filter=>filter.id !== id)
+    setTasks(removeTask)
   }
 
   return (
